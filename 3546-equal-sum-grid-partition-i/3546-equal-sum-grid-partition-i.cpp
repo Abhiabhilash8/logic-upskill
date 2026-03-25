a956f01505cf76b14864cc45b0b5f3557439486a
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-03-25 14:39:59 +0530

    Time: 12 ms (45.68%), Space: 130.3 MB (33.09%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        long long n=grid.size(),m=grid[0].size(),cs=0;
+        vector<long long>h(n,0),v(m,0);
+        for(int i=0;i<n;i++){
+            long long s=0;
+            for(int j=0;j<m;j++) s+=grid[i][j];
+            h[i]=s;
+        }
+        for(int j=0;j<m;j++){
+            long long s=0;
+            for(int i=0;i<n;i++){
+                s+=grid[i][j];
+                cs+=grid[i][j];
+            }
+            v[j]=s;
+        }
+
+        if(cs&1) return false;
+
+        long long yo=0;
+        for(int i=0;i<m;i++){
+            yo+=v[i];
+            if(yo==cs/2) return true;
+        }
+        yo=0;
+        for(int i=0;i<n;i++){
+            yo+=h[i];
+            if(yo==cs/2) return true;
+        }
+
+        return false;
+
+
+
+    }
+};
