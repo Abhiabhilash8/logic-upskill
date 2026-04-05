8a2b39f8ff5e41f44e7c8b4dda48f35cc0b389b4
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:46:11 +0530

    Create 350-IntersectionOfTwoArraysIi

---
+++ 350-IntersectionOfTwoArraysIi/350-IntersectionOfTwoArraysIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/5/2026, 11:46:10 AM
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int>result;
+        for(int begin1=0,begin2=0;begin1<nums1.size()&&begin2<nums2.size();){
+            if(nums1[begin1]==nums2[begin2]){
+                result.push_back(nums1[begin1]);
+                begin1++;
+                begin2++;
+            }
+            else if(nums1[begin1]<nums2[begin2]){
+                begin1++;
+            }
+            else
+            begin2++;
+            
+        }
+        return result;
+    }
+};
