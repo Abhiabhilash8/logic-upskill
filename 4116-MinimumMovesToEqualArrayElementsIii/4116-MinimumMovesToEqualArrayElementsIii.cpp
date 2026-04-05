aa309178fe6322339da1f59ed76778887450afcd
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:41:37 +0530

    Create 4116-MinimumMovesToEqualArrayElementsIii

---
+++ 4116-MinimumMovesToEqualArrayElementsIii/4116-MinimumMovesToEqualArrayElementsIii.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/5/2026, 11:41:36 AM
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int maxi=*max_element(nums.begin(),nums.end());
+
+        int ans=0;
+        for(int i=0;i<nums.size();i++) ans+=(maxi-nums[i]);
+
+        return ans;
+    }
+};
