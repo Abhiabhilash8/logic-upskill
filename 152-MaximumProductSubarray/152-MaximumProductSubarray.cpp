f19f29d24de7fd83b6eb3f1277daf97d7408c838
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:47:22 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/5/2026, 11:47:21 AM
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suf=1,ans=INT_MIN,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(pre==0) pre=1;
+            if(suf==0) suf=1;
+            pre*=nums[i];
+            suf*=nums[n-i-1];
+            ans=max(ans,max(pre,suf));
+        }
+        return ans;
+    }
+};
