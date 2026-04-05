f408dfaeba5fec0259b4c75aa90f1db749122253
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:45:45 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.cpp
@@ -0,0 +1,11 @@
+// Last updated: 4/5/2026, 11:45:44 AM
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned long long >dp(amount+1,0);
+        dp[0]=1;
+        for(int i=0;i<coins.size();i++) for(int j=coins[i];j<=amount;j++) dp[j]+=dp[j-coins[i]];
+ 
+        return dp[amount];
+    }
+};
