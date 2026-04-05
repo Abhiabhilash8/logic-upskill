371493a9ccf5ba41b3230abcebfd7e0fdfa3ddd2
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:44:23 +0530

    Create 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK

---
+++ 1557-CheckIfAStringContainsAllBinaryCodesOfSizeK/1557-CheckIfAStringContainsAllBinaryCodesOfSizeK.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/5/2026, 11:44:22 AM
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if(k>=s.size()) return false;
+        set<string>st;
+        for(int i=0;i<=s.size()-k;i++){
+            st.insert(s.substr(i,k));
+        }
+
+        return st.size()==(1<<k);
+
+    }
+};
