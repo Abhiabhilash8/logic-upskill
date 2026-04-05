18719c6758b166b16e385fe60f118df9d7b47ad5
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-04-05 11:46:34 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/5/2026, 11:46:33 AM
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>result(nums.size(),1);
+       for(int i=1;i<nums.size();i++){
+        result[i]=result[i-1]*nums[i-1];
+       }
+       int x=1;
+       for(int i=nums.size()-2;i>=0;i--){
+        x*=nums[i+1];
+        result[i]*=x;
+       }
+
+
+
+        return result;
+        
+    }
+};
