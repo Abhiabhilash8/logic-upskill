d93b716e3db9ee59d16775cedd6d79abf22dd172
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-05-13 18:43:39 +0530

    Time: 59 ms (25.66%) | Memory: 185.3 MB (19.47%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -5,8 +5,10 @@ public:
         vector<int>dif(2e5+5,0);
         for(int i = 0; i<n/2; i++){
             dif[2]+=2,dif[2*lt + 1] -= 2;
+
             dif[min(nums[i] , nums[n-i-1]) + 1]--;
             dif[max(nums[i] , nums[n-i-1]) + lt + 1]++;
+
             dif[nums[i] + nums[n-i-1]]--;
             dif[nums[i] + nums[n-i-1] + 1]++;
         }
@@ -17,7 +19,6 @@ public:
 
         int ans = INT_MAX;
         for(int i = 2; i<dif.size() && i<=2*lt; i++){
-            cout<<dif[i]<<" ";
             ans = min(ans , dif[i]);
         }
         return ans;
