5cf16ccdd51a2b7bef9541c8b199e0fb5f968935
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-05-13 18:51:47 +0530

    Time: 11 ms (52.21%) | Memory: 93.5 MB (43.36%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minMoves(vector<int>& nums, int lt) {
         int n = nums.size();
-        vector<int>dif(2e5+5,0);
+        vector<int>dif(2*lt+2,0);
         for(int i = 0; i<n/2; i++){
             dif[2]+=2,dif[2*lt + 1] -= 2;
 
@@ -13,14 +13,12 @@ public:
             dif[nums[i] + nums[n-i-1] + 1]++;
         }
 
-        for(int i = 1; i<dif.size();i++){
-            dif[i] += dif[i-1];
-        }
-
         int ans = INT_MAX;
-        for(int i = 2; i<dif.size() && i<=2*lt; i++){
+        for(int i = 2; i<2*lt+1;i++){
+            dif[i] += dif[i-1];
             ans = min(ans , dif[i]);
         }
+
         return ans;
     }
 };
