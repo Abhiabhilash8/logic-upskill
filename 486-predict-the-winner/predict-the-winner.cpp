ff7053aa2fdde3bb055f4c7468a7a2cbb309eb53
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-08-01 21:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (32.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-
+    vector<vector<int>>dp;
     int p(vector<int>&nums , int i , int j , int turn){
+        if(dp[i][j] != -1) return dp[i][j];
         if(i == j) {
             return turn * nums[i];
         }
@@ -9,17 +10,18 @@ public:
             int mk = 0;
             mk = max(mk ,nums[i] + p(nums , i + 1 , j , 0));
             mk = max(mk ,nums[j] + p(nums , i , j - 1 , 0));
-            return mk;
+            return dp[i][j] = mk;
         }else{
             int mk = INT_MAX;
             mk = min(mk , p(nums , i + 1 , j , 1));
             mk = min(mk , p(nums , i , j - 1 , 1));
-            return mk;
+            return dp[i][j] = mk;
         }
 
     }
 
     bool predictTheWinner(vector<int>& nums) {
+        dp.assign(nums.size() , vector<int>(nums.size() , -1));
         return accumulate(nums.begin() , nums.end() , 0) <= 2* p(nums , 0 , nums.size() - 1 , 1);
     }
 };
