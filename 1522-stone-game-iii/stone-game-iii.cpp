9660594907e86860881c77bd311a666ac94daf34
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-08-03 12:11:07 +0530

    Time: 27 ms (64.47%) | Memory: 142 MB (36.35%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& sv) {
         int n = sv.size();
-        vector<vector<int>>dp(n + 1 , vector<int>(2 , 0));
+        vector<int>dp(n + 1 , 0);
         vector<int>rs(n + 1 , 0);
         rs[n] = 0;
         for(int i = n-1; i>=0; i--) rs[i] = rs[i+1] + sv[i];
         for(int i = n-1; i>=0; i--){
-            for(int turn = 0; turn < 2; turn++){
-                dp[i][turn] = sv[i] + rs[i+1] - dp[i + 1][!turn];
-                if(i + 1 < n) dp[i][turn] = max(dp[i][turn] , sv[i] + sv[i+1] + rs[i+2] - dp[i+2][!turn]);
-                if(i + 2 < n) dp[i][turn] = max(dp[i][turn] , sv[i] + sv[i+1] + sv[i+2] + rs[i+3] - dp[i+3][!turn]);
-            }
+                dp[i] = sv[i] + rs[i+1] - dp[i + 1];
+                if(i + 1 < n) dp[i] = max(dp[i] , sv[i] + sv[i+1] + rs[i+2] - dp[i+2]);
+                if(i + 2 < n) dp[i] = max(dp[i] , sv[i] + sv[i+1] + sv[i+2] + rs[i+3] - dp[i+3]);
         }
 
         int ts = accumulate(sv.begin() , sv.end() , 0);
-        if(2 * dp[0][1] > ts) return "Alice";
-        else if(2 * dp[0][1] < ts) return "Bob";
+        if(2 * dp[0] > ts) return "Alice";
+        else if(2 * dp[0] < ts) return "Bob";
         else return "Tie";
     }
 };
