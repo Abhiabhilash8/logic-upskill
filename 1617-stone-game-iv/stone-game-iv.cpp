97d5147265485f7c2911cedfeba64ec9d322a6b4
Sadhu Abhilash <abhiabhiiii699@gmail.com>
2026-08-10 11:06:44 +0530

    Time: 23 ms (89.10%) | Memory: 9 MB (73.64%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
+
 /*
-    dp[i][t] = false -> dp[i + any sq][!t] == true
-    dp[i][t] = true ->  dp[i + any sq][!t] == false
-
-    dp[0][0] = 1 
-
-    f(i , turn){
-
-    }
-
-    turn == 1 alice turn
-    else bob turn
+    dp[i] = winning state of cur player when i stones are remaining 
+    for any s if dp[i-s] == false, --> prev player loose, hence cur player wins 
 */
 
-    vector<int>sq;
-    vector<vector<int>>dp;
-    int k;
-    bool f(int i , int turn){
-        if(i > k) return turn;
-        if(dp[i][turn] != -1) return dp[i][turn];
-        bool a = false, b = true;
-        for(auto s: sq){
-            if(i + s > k) break;
-            if(turn) a |= f(i + s , !turn);
-            else b &= f(i + s , !turn);
+    bool winnerSquareGame(int n) {
+        vector<int>sq;
+        for(int i = 1; i*i <= n; i++) sq.push_back(i*i);
+        vector<bool>dp(n +1 , false);
+
+        dp[0] = false;          //cur player loose when 0 stones are left
+        for(int i = 1; i<=n; i++){
+            for(auto s: sq){
+                if(i - s < 0) break;
+                if(!dp[i - s]){
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
 
-        if(turn) return dp[i][turn] = a;
-        else return dp[i][turn] = b;
-
-    }
-
-    bool winnerSquareGame(int n) {
-        k = n;
-        for(int i = 1; i*i <= n; i++) sq.push_back(i*i);
-        
-        dp.assign(n+1 , vector<int>(2 , -1));
-
-        return f(0 , 1);
+        return dp[n];
     }
 };
